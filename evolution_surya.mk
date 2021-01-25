c5761446c24e5fe947f5e0ab670a6ff8f2860da4
Akash Kakkar <akash.galaxy07@gmail.com>
2021-01-25 14:42:42 +0530

    We don't need shitty "official" tag now. Let's go custom

--- evolution_surya.mk
+++ evolution_surya.mk
@@ -21,10 +21,11 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_WIFI_EXT := true
 
 #Officialization
-EVO_BUILD_TYPE := OFFICIAL
+#EVO_BUILD_TYPE := OFFICIAL
 EVO_MAINTAINER := AkashKakkar
 EVO_SUPPORT_URL := https://t.me/EvolutionXSurya
 EVO_DONATE_URL := https://www.paypal.me/akash07
+CUSTOM_BUILD_TYPE := Squeezers_Adition
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := evolution_surya
@@ -35,8 +36,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := "POCO/surya_global/surya:10/QKQ1.200512.002V12.0.8.0.QJGINXM:user/release-keys"
 TARGET_INCLUDE_PIXEL_CHARGER := true
-
-#Kept for fallback
-# CUSTOM_BUILD_TYPE := UNOFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
